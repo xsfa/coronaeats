28a940a8a68b3551d678d7df7915b5bee9e51c04
zestyshenmutz <tesfashenkute@tesfaMBP.hsd1.wa.comcast.net>
2020-10-17 17:43:23 -0700

    added map and corners

--- main.js
+++ main.js
@@ -1,50 +1,10 @@
-//for navbar
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".navLinks");
-    const navLinks = document.querySelectorAll(".navLinks li");
-    
-    burger.addEventListener('click', () => {
-        //toggle nav
-        nav.classList.toggle("navActive");
-        //animate links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ''
-            }
-            else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-        //burger animation
-        burger.classList.toggle("toggle");
-        
-    });
-    
-};
-
-
-var modal = document.getElementById("popup");
-var btn = document.getElementById("contact");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
+//for map
+function initMap() {
+  // The location of Seattle
+  const seattle = { lat: 47.6062, lng: 122.3321 };
+  // The map, centered at Seattle
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: seattle,
+  });
 }
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
-const app = () => {
-    navSlide();
-};
-app();
