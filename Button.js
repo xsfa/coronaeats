ac447518f94112d44164c2c708e1719455887f10
zestyshenmutz <tesfashenkute@tesfaMBP.hsd1.wa.comcast.net>
2020-10-18 00:25:49 -0700

    let me pull

--- Button.js
+++ Button.js
@@ -1,13 +1,11 @@
-const path = require('path');
-const Path = path.join(__dirname, 'urls.txt');
+//const Path = path.join(__dirname, 'urls.txt');
 
 var text = fs.readFileSync("./urls.txt").toString('utf-8');
 var txtnolinebreaks = text.replace(/(\r\n|\n|\r)/gm,"");
 var urls = txtnolinebreaks.split(",");
 
 function runme() { 
-  
   var value = urls[Math.floor(Math.random() * urls.length)];
-  //alert("Will navigate to: " + value);
-  window.location = value;     
+  alert("Will navigate to: " + value);
+  window.location = value;
 }
